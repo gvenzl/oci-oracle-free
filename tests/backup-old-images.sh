e0b21c6f0b48225d6e510acb07e55aae04cf2fc8
Gerald Venzl <gerald.venzl@gmail.com>
2024-12-07 12:14:50 -0800

    Refactor backup of old images
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- tests/backup-old-images.sh
+++ tests/backup-old-images.sh
@@ -25,42 +25,20 @@ set -Eeuo pipefail
 
 source ./functions.sh
 
-# Download images
+########################################
+### Backup latest images
+########################################
 echo "Backup latest"
 backupImage "docker.io" "latest-$(getArch)"
 
 echo "Backup latest-faststart"
 backupImage "docker.io" "latest-faststart-$(getArch)"
 
-# Backup images
-echo "Backup 23.5-full-faststart"
-backupImage "docker.io" "23.5-full-faststart-$(getArch)"
-
-echo "Backup 23.5-full"
-backupImage "docker.io" "23.5-full-$(getArch)"
 
 
-# Backup images
-#echo "Backup 23.4-full-faststart"
-#backupImage "docker.io" "23.4-full-faststart"
-
-#echo "Backup 23.4-full"
-#backupImage "docker.io" "23.4-full"
-
-#echo "Backup 23.3-full-faststart"
-#backupImage "docker.io" "23.3-full-faststart"
-
-#echo "Backup 23.3-full"
-#backupImage "docker.io" "23.3-full"
-
-#echo "Backup 23.2-full-faststart"
-#backupImage "docker.io" "23.2-full-faststart"
-
-#echo "Backup 23.2-full"
-#backupImage "docker.io" "23.2-full"
-
-
-
+########################################
+### Backup full images
+########################################
 echo "Backup 23-full-faststart"
 backupImage "docker.io" "23-full-faststart-$(getArch)"
 
@@ -75,133 +53,145 @@ backupImage "docker.io" "full-$(getArch)"
 
 
 
-echo "Backup 23.5-faststart"
-podman pull docker.io/gvenzl/oracle-free:23.5-faststart-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23.5-faststart-$(getArch) docker.io/gvenzl/oracle-free:23.5-faststart-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23.5-faststart-$(getArch)
-
-echo "Backup 23.5"
-podman pull docker.io/gvenzl/oracle-free:23.5-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23.5-$(getArch) docker.io/gvenzl/oracle-free:23.5-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23.5-$(getArch)
-
-
-echo "Backup 23.4-faststart"
-#podman pull docker.io/gvenzl/oracle-free:23.4-faststart
-#podman tag  docker.io/gvenzl/oracle-free:23.4-faststart docker.io/gvenzl/oracle-free:23.4-faststart-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.4-faststart
-
-#echo "Backup 23.4"
-#podman pull docker.io/gvenzl/oracle-free:23.4
-#podman tag  docker.io/gvenzl/oracle-free:23.4 docker.io/gvenzl/oracle-free:23.4-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.4
-
-
-#echo "Backup 23.3-faststart"
-#podman pull docker.io/gvenzl/oracle-free:23.3-faststart
-#podman tag  docker.io/gvenzl/oracle-free:23.3-faststart docker.io/gvenzl/oracle-free:23.3-faststart-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.3-faststart
-
-#echo "Backup 23.3"
-#podman pull docker.io/gvenzl/oracle-free:23.3
-#podman tag  docker.io/gvenzl/oracle-free:23.3 docker.io/gvenzl/oracle-free:23.3-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.3
-
-
-#echo "Backup 23.2-faststart"
-#podman pull docker.io/gvenzl/oracle-free:23.2-faststart
-#podman tag  docker.io/gvenzl/oracle-free:23.2-faststart docker.io/gvenzl/oracle-free:23.2-faststart-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.2-faststart
-
-#echo "Backup 23.2"
-#podman pull docker.io/gvenzl/oracle-free:23.2
-#podman tag  docker.io/gvenzl/oracle-free:23.2 docker.io/gvenzl/oracle-free:23.2-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.2
-
-
-
-
-
-
+########################################
+### Backup regular images
+########################################
 echo "Backup 23-faststart"
-podman pull docker.io/gvenzl/oracle-free:23-faststart-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23-faststart-$(getArch) docker.io/gvenzl/oracle-free:23-faststart-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23-faststart-$(getArch)
+backupImage "docker.io" "23-faststart-$(getArch)"
 
 echo "Backup 23"
-podman pull docker.io/gvenzl/oracle-free:23-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23-$(getArch) docker.io/gvenzl/oracle-free:23-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23-$(getArch)
+backupImage "docker.io" "23-$(getArch)"
 
 
 
-
-
-
-echo "Backup 23.5-slim-faststart"
-podman pull docker.io/gvenzl/oracle-free:23.5-slim-faststart-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23.5-slim-faststart-$(getArch) docker.io/gvenzl/oracle-free:23.5-slim-faststart-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23.5-slim-faststart-$(getArch)
-
-echo "Backup 23.5-slim"
-podman pull docker.io/gvenzl/oracle-free:23.5-slim-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23.5-slim-$(getArch) docker.io/gvenzl/oracle-free:23.5-slim-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23.5-slim-$(getArch)
-
-
-#echo "Backup 23.4-slim-faststart"
-#podman pull docker.io/gvenzl/oracle-free:23.4-slim-faststart
-#podman tag  docker.io/gvenzl/oracle-free:23.4-slim-faststart docker.io/gvenzl/oracle-free:23.4-slim-faststart-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.4-slim-faststart
-
-#echo "Backup 23.4-slim"
-#podman pull docker.io/gvenzl/oracle-free:23.4-slim
-#podman tag  docker.io/gvenzl/oracle-free:23.4-slim docker.io/gvenzl/oracle-free:23.4-slim-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.4-slim
-
-
-#echo "Backup 23.3-slim-faststart"
-#podman pull docker.io/gvenzl/oracle-free:23.3-slim-faststart
-#podman tag  docker.io/gvenzl/oracle-free:23.3-slim-faststart docker.io/gvenzl/oracle-free:23.3-slim-faststart-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.3-slim-faststart
-
-#echo "Backup 23.3-slim"
-#podman pull docker.io/gvenzl/oracle-free:23.3-slim
-#podman tag  docker.io/gvenzl/oracle-free:23.3-slim docker.io/gvenzl/oracle-free:23.3-slim-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.3-slim
-
-
-#echo "Backup 23.2-slim-faststart"
-#podman pull docker.io/gvenzl/oracle-free:23.2-slim-faststart
-#podman tag  docker.io/gvenzl/oracle-free:23.2-slim-faststart docker.io/gvenzl/oracle-free:23.2-slim-faststart-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.2-slim-faststart
-
-#echo "Backup 23.2-slim"
-#podman pull docker.io/gvenzl/oracle-free:23.2-slim
-#podman tag  docker.io/gvenzl/oracle-free:23.2-slim docker.io/gvenzl/oracle-free:23.2-slim-backup
-#podman rmi  docker.io/gvenzl/oracle-free:23.2-slim
-
-
-
-
-
-
+########################################
+### Backup slim images
+########################################
 echo "Backup 23-slim-faststart"
-podman pull docker.io/gvenzl/oracle-free:23-slim-faststart-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23-slim-faststart-$(getArch) docker.io/gvenzl/oracle-free:23-slim-faststart-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23-slim-faststart-$(getArch)
+backupImage "docker.io" "23-slim-faststart-$(getArch)"
 
 echo "Backup 23-slim"
-podman pull docker.io/gvenzl/oracle-free:23-slim-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:23-slim-$(getArch) docker.io/gvenzl/oracle-free:23-slim-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:23-slim-$(getArch)
+backupImage "docker.io" "23-slim-$(getArch)"
 
 echo "Backup slim-faststart"
-podman pull docker.io/gvenzl/oracle-free:slim-faststart-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:slim-faststart-$(getArch) docker.io/gvenzl/oracle-free:slim-faststart-$(getArch)-backup
-podman rmi  docker.io/gvenzl/oracle-free:slim-faststart-$(getArch)
+backupImage "docker.io" "slim-faststart-$(getArch)"
 
 echo "Backup slim"
-podman pull docker.io/gvenzl/oracle-free:slim-$(getArch)
-podman tag  docker.io/gvenzl/oracle-free:slim-$(getArch) docker.io/gvenzl/oracle-free:slim-backup
-podman rmi  docker.io/gvenzl/oracle-free:slim-$(getArch)
+backupImage "docker.io" "slim-$(getArch)"
+
+
+
+
+########################################
+### Backup 23.6 images
+########################################
+echo "Backup 23.6-full-faststart"
+backupImage "docker.io" "23.6-full-faststart-$(getArch)"
+
+echo "Backup 23.6-full"
+backupImage "docker.io" "23.6-full-$(getArch)"
+
+echo "Backup 23.6-faststart"
+backupImage "docker.io" "23.6-faststart-$(getArch)"
+
+echo "Backup 23.6"
+backupImage "docker.io" "23.6-$(getArch)"
+
+echo "Backup 23.6-slim-faststart"
+backupImage "docker.io" "23.6-slim-faststart-$(getArch)"
+
+echo "Backup 23.6-slim"
+backupImage "docker.io" "23.6-slim-$(getArch)"
+
+
+
+
+########################################
+### Backup 23.5 images
+########################################
+echo "Backup 23.5-full-faststart"
+backupImage "docker.io" "23.5-full-faststart-$(getArch)"
+
+echo "Backup 23.5-full"
+backupImage "docker.io" "23.5-full-$(getArch)"
+
+echo "Backup 23.5-faststart"
+backupImage "docker.io" "23.5-faststart-$(getArch)"
+
+echo "Backup 23.5"
+backupImage "docker.io" "23.5-$(getArch)"
+
+echo "Backup 23.5-slim-faststart"
+backupImage "docker.io" "23.5-slim-faststart-$(getArch)"
+
+echo "Backup 23.5-slim"
+backupImage "docker.io" "23.5-slim-$(getArch)"
+
+
+
+########################################
+### Backup 23.4 images
+########################################
+echo "Backup 23.4-full-faststart"
+backupImage "docker.io" "23.4-full-faststart"
+
+echo "Backup 23.4-full"
+backupImage "docker.io" "23.4-full"
+
+echo "Backup 23.4-faststart"
+backupImage "docker.io" "23.4-faststart"
+
+echo "Backup 23.4"
+backupImage "docker.io" "23.4"
+
+echo "Backup 23.4-slim-faststart"
+backupImage "docker.io" "23.4-slim-faststart"
+
+echo "Backup 23.4-slim"
+backupImage "docker.io" "23.4-slim"
+
+
+
+########################################
+### Backup 23.3 images
+########################################
+echo "Backup 23.3-full-faststart"
+backupImage "docker.io" "23.3-full-faststart"
+
+echo "Backup 23.3-full"
+backupImage "docker.io" "23.3-full"
+
+echo "Backup 23.3-faststart"
+backupImage "docker.io" "23.3-faststart"
+
+echo "Backup 23.3"
+backupImage "docker.io" "23.3"
+
+echo "Backup 23.3-slim-faststart"
+backupImage "docker.io" "23.3-slim-faststart"
+
+echo "Backup 23.3-slim"
+backupImage "docker.io" "23.3-slim"
+
+
+
+########################################
+### Backup 23.2 images
+########################################
+echo "Backup 23.2-full-faststart"
+backupImage "docker.io" "23.2-full-faststart"
+
+echo "Backup 23.2-full"
+backupImage "docker.io" "23.2-full"
+
+echo "Backup 23.2-faststart"
+backupImage "docker.io" "23.2-faststart"
+
+echo "Backup 23.2"
+backupImage "docker.io" "23.2"
+
+echo "Backup 23.2-slim-faststart"
+backupImage "docker.io" "23.2-slim-faststart"
+
+echo "Backup 23.2-slim"
+backupImage "docker.io" "23.2-slim"
